1658c1bb883699f3fc7c508aa195844d9e348fef
npzspp-wq <npzspp@gmail.com>
2026-09-23 15:19:43 +0300

    Bump product-first segmentation build to 2026.09.23.04

--- cheviplus_license_resilience.py
+++ cheviplus_license_resilience.py
@@ -20,7 +20,7 @@ import cheviplus_license as lic
 import cheviplus_ui_cleanup_532 as ui
 
 APP_VERSION = "5.36"
-APP_BUILD = "2026.09.23.03"
+APP_BUILD = "2026.09.23.04"
 GRACE_DAYS = 30
 WARN_DAYS = 30
 
